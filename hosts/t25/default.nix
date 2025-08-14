e0913f56db564240de117c13a78f8a53550966e2
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2025-08-14 11:21:19 -0700

    gnome: enable fractional scaling

--- hosts/t25/default.nix
+++ hosts/t25/default.nix
@@ -54,7 +54,13 @@
 
   # Enable GNOME Desktop Environment
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome = {
+    enable = true;
+    extraGSettingsOverrides = ''
+      [org.gnome.mutter]
+      experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
+    '';
+  };
   services.gnome.core-apps.enable = false;
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
